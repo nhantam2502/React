240cd0277a759138e8aff022e1b44cfd812e6a18
Nhan Tam <nhantamhd@gmail.com>
2023-06-07 00:54:11 +0700

    Components Part 2

--- confusion/src/components/Players.js
+++ confusion/src/components/Players.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Players } from '../shared/ListOfPlayers'
+import React, {Component} from 'react'
+import { Player } from '../shared/ListOfPlayers'
 export default class Players extends Component {
     render() {
         return (
             <div className='container'>
-                {Players.map((player) => (
+                {Player.map((player) => (
                     <div className='column'>
                         <div className='card'>
                             <img src={player.img} />
                             <h3>{player.name}</h3>
                             <p className='title'>{player.club}</p>
-                            <p><button>Detail</button></p>
+                            <p className='btn__detail'><button>Detail</button></p>
                         </div>
                     </div>
                 ))}
